e69f3bf8e02cf72f35111236d605a525d94be444
Jonathan Pope <jonathanmpope@gmail.com>
2022-04-21 11:20:35 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -6,18 +6,23 @@
 # No arguments are passed; upcase has one clear job which is to capitalize all letters that exist in the String
 # The return value is "HI"
 "Hello World".upcase
-
+#The include method tests if the argument "Hello" is in the object it's called on.
+#It evaluates to a boolean (true or false)
+#The returned value was true
 
 "Hello World".include?("Hello")
-
-
+#The end.with? method tests if the argument, "Hello" in this case, is how the object it's called on ends.
+#It evaluates to a boolean. This return value is false.
 "Hello World".end_with?("Hello")
-
-
+#The same method as above, so the same explanation works.
+#This will return a value of true because the last 3 letters of the argument are the last three of the object.
 "Hello World".end_with?("rld")
-
+#This method measures how long the object is (in characters). The return will always be an integer.
+#There is no argument, because it's being called on the object directly.
+# This will return a value of 11
 
 "Hello World".length
-
+#This method does the exact same thing as .length and will return the character length of the object in an integer.
+# The return value is 11. 
 
 "Hello World".size
